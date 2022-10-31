a6411c4186494902cb62c7cfd1f650f74fb51f9c
Yurii-Isaev <devhouse27@gmail.com>
2022-10-31 19:34:12 +0200

    basic: concurrency methods

---
+++ basics/core/src/main/java/concurrency/_Runnable.java
@@ -0,0 +1,33 @@
+package concurrency;
+
+public class _Runnable implements Runnable {
+
+   private String name;
+
+   public _Runnable(String name) {
+      this.name = name;
+   }
+
+   @Override
+   public void run() {
+      System.out.println("run() " + name + " running");
+   }
+
+   public static void main(String[] args) {
+      Thread thread1 = new Thread(new _Runnable("1"));
+      Thread thread2 = new Thread(new _Runnable("2"));
+      Thread thread3 = new Thread(new _Runnable("3"));
+      Thread thread4 = new Thread(new _Runnable("4"));
+      Thread thread5 = new Thread(new _Runnable("5"));
+      Thread thread6 = new Thread(new _Runnable("6"));
+      Thread thread7 = new Thread(new _Runnable("7"));
+      thread7.setPriority(Thread.MAX_PRIORITY);
+      thread1.start();
+      thread2.start();
+      thread3.start();
+      thread4.start();
+      thread5.start();
+      thread6.start();
+      thread7.start();
+   }
+}
---
+++ basics/core/src/main/java/concurrency/task/Counter.java
@@ -0,0 +1,36 @@
+package concurrency.task;
+
+/**
+ * Задание:
+ * 1. Создать два счетчика (внутренний и глобальный);
+ * 2. Создать и запустить два потока и перебрать цыкл из 20 элементов;
+ */
+
+public class Counter implements Runnable{
+   private int count;
+   private static int globalCount;
+
+   @Override
+   public void run() {
+      while (count < 20) {
+         count++;
+         globalCount++; // 60
+
+         try {
+            Thread.sleep(100); // создает рассинхрон потоков
+         } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+         }
+
+         System.out.println(Thread.currentThread().getName() +
+             " counter " + count +
+             " globalCount: " + globalCount);
+      }
+   }
+
+   public static void main(String[] args) {
+      new Thread(new Counter()).start();
+      new Thread(new Counter()).start();
+      new Thread(new Counter()).start();
+   }
+}
